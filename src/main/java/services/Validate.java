b7a930c08f86ecef1dd3e11823d58846013a3409
UGO <ublanquet@e-biz.fr>
2017-03-17 14:08:20 +0100

    [refactor] fixed validate checktyle

--- src/main/java/services/Validate.java
+++ src/main/java/services/Validate.java
@@ -14,15 +14,16 @@ public class Validate {
 
     /**
      * check valid string to long.
+     *
      * @param input input string
      * @return is valid long ?
      */
-    public static Boolean isValidLong(String input){
+    public static Boolean isValidLong(String input) {
         long longInput;
         try {
             longInput = Long.parseLong(input);
         } catch (NumberFormatException ex) {
-            logger.error("Long validation error, input : "+input);
+            logger.error("Long validation error, input : " + input);
             return false;
         }
 
@@ -31,15 +32,16 @@ public class Validate {
 
     /**
      * parse a long from string.
+     *
      * @param input input
      * @return converted number or null
      */
-    public static long parseLong(String input){
+    public static long parseLong(String input) {
         Long longInput = null;
         try {
             longInput = Long.parseLong(input);
         } catch (NumberFormatException ex) {
-            logger.error("Long parsing error, input : "+input);
+            logger.error("Long parsing error, input : " + input);
         }
 
         return longInput;
@@ -47,10 +49,11 @@ public class Validate {
 
     /**
      * parse a date, format dd/MM/yyyy.
+     *
      * @param input input string
      * @return date obj or null if invalid
      */
-    public static LocalDateTime parseDate(String input){
+    public static LocalDateTime parseDate(String input) {
         LocalDateTime date = null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -59,25 +62,26 @@ public class Validate {
             if (!Objects.equals(input, "0") && !Objects.equals(input, "")) {
                 date = LocalDate.parse(input, formatter).atStartOfDay();
             }
-        }catch (DateTimeException ex){
-                logger.error("Date validation error, check dates " + ex.getMessage());
-            }
+        } catch (DateTimeException ex) {
+            logger.error("Date validation error, check dates " + ex.getMessage());
+        }
 
         return date;
     }
 
     /**
      * check if a date is valid.
+     *
      * @param input input
      * @return true/false
      */
-    public static Boolean isValidDate(String input){
+    public static Boolean isValidDate(String input) {
         LocalDateTime date = null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         try {
             date = LocalDate.parse(input, formatter).atStartOfDay();
-        }catch (DateTimeException ex){
-            logger.error("Date validation error, check input :" + input + ex.getMessage() );
+        } catch (DateTimeException ex) {
+            logger.error("Date validation error, check input :" + input + ex.getMessage());
             return false;
         }
 
