e7b86f7fb0416a2fcee1b5bc48ddbed2664e2d83
UGO <ublanquet@e-biz.fr>
2017-03-23 11:29:42 +0100

    [test]added selenium, not working (versions msimatchs)

--- src/main/webapp/views/addComputer.jsp
+++ src/main/webapp/views/addComputer.jsp
@@ -40,6 +40,7 @@
                             <label for="discontinued">Discontinued date</label>
                             <input type="date" class="form-control" name="discontinued" id="discontinued" placeholder="Discontinued date (dd/mm/yyyy)" pattern="^(((0[1-9]|[12]\d|3[01])/(0[13578]|1[02])/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)/(0[13456789]|1[012])/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])/02/((19|[2-9]\d)\d{2}))|(29/02/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$">
                         </div>
+                        <!-- ^[0-3][0-9][-][0-1][0-9][-](([1][9][9][0-9])|([2-9][0-9]{3}))$ -->
                         <div class="form-group">
                             <label for="companyId">Company</label>
                             <select class="form-control" name="companyId" id="companyId">
---
+++ src/test/java/Selenium/SeleniumTests.java
@@ -0,0 +1,88 @@
+package Selenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.ArrayList;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class SeleniumTests {
+  WebDriver driver;
+
+  @Before
+  public void setUp() {
+    System.setProperty("webdriver.gecko.driver", "/home/ebiz/Téléchargements/geckodriver-v0.14");
+    driver = new FirefoxDriver();
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  //@Test
+  public void test() throws Exception {
+    // Create a new instance of the Firefox driver
+    // Notice that the remainder of the code relies on the interface,
+    // not the implementation.
+
+
+    // And now use this to visit Google
+    driver.get("http://www.google.com");
+    // Alternatively the same thing can be done like this
+    // driver.navigate().to("http://www.google.com");
+
+    // Find the text input element by its name
+    WebElement element = driver.findElement(By.name("q"));
+
+    // Enter something to search for
+    element.sendKeys("Cheese!");
+
+    // Now submit the form. WebDriver will find the form for us from the element
+    element.submit();
+
+    // Check the title of the page
+    System.out.println("Page title is: " + driver.getTitle());
+
+    // Google's search is rendered dynamically with JavaScript.
+    // Wait for the page to load, timeout after 10 seconds
+    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+      public Boolean apply(WebDriver d) {
+        return d.getTitle().toLowerCase().startsWith("cheese!");
+      }
+    });
+
+    /*(new WebDriverWait(driver, 10)).until(new Function<WebDriver, Boolean>() {
+      public Boolean apply(WebDriver d) {
+        return d.getTitle().toLowerCase().startsWith("cheese!");
+      }
+    });
+    (new WebDriverWait(driver, 10)).until(d -> d.getTitle().toLowerCase().startsWith("cheese!"));*/
+
+    // Should see: "cheese! - Google Search"
+    System.out.println("Page title is: " + driver.getTitle());
+
+    //Close the browser
+    driver.quit();
+  }
+
+  public WebElement getWhenVisible(By locator, int timeout) {
+    WebElement element = null;
+    WebDriverWait wait = new WebDriverWait(driver, timeout);
+    element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    return element;
+  }
+  public void clickWhenReady(By locator, int timeout) {
+    WebDriverWait wait = new WebDriverWait(driver, timeout);
+    WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+    element.click();
+  }
+}
