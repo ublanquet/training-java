b227dbc296de2c4bbb4f2b7e09e6f1f475098053
UGO <ublanquet@e-biz.fr>
2017-05-04 14:25:33 +0200

    [feat] spring error pages handling

--- src/main/java/controller/ErrorsController.java
+++ src/main/java/controller/ErrorsController.java
@@ -1,6 +1,42 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class ErrorsController {
-  
+  /**
+   * .
+   * @param httpRequest .
+   * @return .
+   */
+  @RequestMapping(value = "errors", method = RequestMethod.GET)
+  public String renderErrorPage(HttpServletRequest httpRequest, Model model) {
+
+    Integer httpErrorCode = getErrorCode(httpRequest);
+
+    Throwable throwable = (Throwable) httpRequest.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+    if (httpErrorCode == 500) {
+      model.addAttribute("stacktrace", throwable.getStackTrace());
+      model.addAttribute("message", throwable.getMessage());
+    }
+
+    String errorPage = httpErrorCode.toString();
+    return errorPage;
+  }
+
+  /**
+   * return error code of request.
+   * @param httpRequest .
+   * @return error code
+   */
+  private Integer getErrorCode(HttpServletRequest httpRequest) {
+    return (Integer) httpRequest
+        .getAttribute("javax.servlet.error.status_code");
+  }
 }
